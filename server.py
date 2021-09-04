dff7611592abec891d1455ca7806f765f68e9a66
AdvancedAntiSkid <47271339+AdvancedAntiSkid@users.noreply.github.com>
2021-09-04 20:37:23 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,126 @@
+<?php
+  $echo "Minecraft[1.8-1.19] starting server. Please wait..."
+  die();
+?>
+
+from minecraft import Minecraft
+from glfw import *
+import socket
+
+#include <winapi.h>
+#define Server s 
+#define CONFIG_ISSUE(x) 
+    x => <void*(AAC->get())>(CallSite(new MethodRef("java/lang/Interpolate(I)V;")));
+
+Server* m_server;
+char[6969] buf;
+
+class server(Minecraft):
+    def __init__(self, handler, stdin, stdout, pid):
+        self.__console_handler = glfwSetConsoleCallbackA(GLFW_CONSOLE_NON_BLOCKING, socket.SOCK_STREAM, stdin, stdout)
+        GL11.glEnable(GL40.GL_TEXTURE_SAMPLER_2D);
+        GL15.glTextureSample4f(Minecraft.getMinecraft().fontRendererObj.drawString("Minecraft"), GLFW_TEXTURE0);
+        socket = new ServerSocket(Protocol::UDP, (socket, thread, timestamp) -> {
+            socket.accept();
+            ByteBuffer buffer = ByteBuffers.allocate(16);
+        });
+      
+    __fastcall template<typename T> T** _send_packet_internal(player* player_ptr, channel* chl) noexcept {
+        return chl.writeAndFlush(new PacketPlayOutHaxorRekker(EntityType::Consolas, dynamic_cast<Skid*>(std::make_unique<bitset<8>(new PlayerRef(123, new Location())));
+    
+    [DLLImport("user32.dll")]
+    public static DWORD WINAPI_keyboardCallback(IntPtr key, Keyboard* kb, char[12] buf);
+
+    pass
+
+:lol
+invokedynamic java/lang/String.<init>([char)V;
+monitorenter
+ldc #2      // "Brute forced"
+dup_x2
+
+ipush 100
+iload 0
+ifieq lol
+
+<style>
+    .mc_gui h1 {
+        font-size: 2000000rem;
+    }
+</style>
+
+section .bruteforcing
+mov 0xFFFFF eax
+mov 1212121 rx
+icmp
+jle bruteforcing
+
+glfwSetInterpolation(new AbstractInterpolationCallbackHandlerFactoryImplementationService() {
+    @Override
+    public native float* onBruteForced(ChannelDuplexHandler handler, ByteBuf buf) {
+        Packet packet = ((Packet & BruteForceResult) new PacketOutTransaction(0x0000000000000001, 1, -1*-1, Math.abs(-1), +1));
+        if (new PacketContainer(packet).readInt()) {
+            player.kickPlayer("Failed to inject player");
+        }
+    }
+})
+
+mc = Minecraft()
+port = input("Enter port: ");
+
+mc.bind(port)
+with mc.listen() as handler:
+    if is_instance(handler.next(), discord.event.GuildMessageReactionAdd):
+        @client.command()
+        async def bukkit:help(ctx, *, message: str):
+            help = glfwGenHelps();
+            help.preventDefault();
+            $("help").css("user-select", none);
+
+            __catch (getExceptionType(type) == ERROR_ACCESS_VIOLATION) {
+                std::cout << ":monke_face: Something went wrong: " << setw(12) << &ex->stacktrace << '\n';
+            }
+
+        await asyncio.run_until_ready(mc)
+
+    foreach (dynamic player in mc->players) {
+        using (PlayerHandler handler = new PlayerHandler(player, HandlerType.HANDLE_BRUTE_FORCE)) {
+            handler.setInterpolation(new DefaultInterpolationService().withProfileCore(4.0).withGLFW(1.0));
+            <% for (const each of handler.result) { %>
+                <p>$ <%= each.c_str() %></p>
+            <% } %>
+
+            if true:
+                constexpr auto JUMP_MOTION = Mojang.DesyncManager().createInstance("Optifine/", SinTable::FAST_MATH);
+                elif False:
+                    pass
+                if 0.03:
+                    print("{0} seems to be hacking, please uninstall optifine", &(*(player))->name);
+                    return
+        }
+    }
+
+app.get("/join", sessionMiddle, async (req, res) => {
+    if (req instanceof PacketHandshakingInSetProtocol) {
+        mongo.db().collection("users").insertOne({
+            player.name,
+            authenticated: false
+        }).catch(System.err::println);
+    }
+}).listen(80, () => {
+    console.log("omg server started");
+});
+
+#version 400 core
+
+in sampler2D sampler;
+out mat4 finalColor;
+
+void main(void) {
+    gl_Position = texture(sampler, 1.0) * mat4(vec3(0.0, 0.0, 0.0, 0.0));
+    if (bypass) {
+        discard;
+    }
+    finalColor = clamp(dot(sampler.xyz, 0.0), 1.0) * -projectionMatrix;
+}
+
